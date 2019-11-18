dea272efaabb5e077e60e135b9419144d0b056ac
yuu30 <y.nagai0330@gmail.com>
2019-11-19 03:00:00 +0900

    buildディレクトリをdocsに変更するようpackage.jsonを修正

---
+++ docs/precache-manifest.22c18be0b48277edb9bf3dae186412dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010377918772cc9d9318d09c2876ad82",
+    "url": "/issues/index.html"
+  },
+  {
+    "revision": "888a55017eed1a81b9b2",
+    "url": "/issues/static/css/main.497a3a7d.chunk.css"
+  },
+  {
+    "revision": "1fcbc212c19884fab9dd",
+    "url": "/issues/static/js/2.89cdd476.chunk.js"
+  },
+  {
+    "revision": "888a55017eed1a81b9b2",
+    "url": "/issues/static/js/main.0019ab67.chunk.js"
+  },
+  {
+    "revision": "b59a3017498a12f072cc",
+    "url": "/issues/static/js/runtime-main.a8eb5704.js"
+  }
+]);
